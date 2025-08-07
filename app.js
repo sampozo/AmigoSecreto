5b231220a4b93f96c0803a8b54a0dfb64641065d
Spin giaas <spingiaas@gmail.com>
2025-08-07 19:30:09 -0400

    commit 1: Agregar amigos y sortear amigo

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-// Codigo app.js para Challenge "Amigo Secreto"
-// agregando código por partes haciendo COMMITS
+// Código básico para una aplicación de sorteo de amigo secreto.
+// Variable tipo array para almacenar los nombres de los amigos.
 
+let amigos = [];
+
+// --- Función para agregar amigos a la lista ---
+
+function agregarAmigo() {
+
+    // Obtener el texto ingresado por el usuario del input con ID "amigo".
+
+    let nombre = document.getElementById('amigo').value;
+
+    // Validar la entrada. Si el campo está vacío, mostrar una alerta.
+
+    if (nombre.trim() === "") {
+        alert("Por favor, inserte un nombre.");
+        return; // Detenemos la función para no agregar un valor vacío.
+    }
+
+    // Actualizar el array de amigos usando .push().
+
+    amigos.push(nombre);
+
+    // Limpiar el campo de entrada.
+
+    document.getElementById('amigo').value = '';
+
+    // Llamamos a la función mostrarAmigosEnLista para mostrar la lista actualizada en la página.
+
+    mostrarAmigosEnLista();
+}
+
+//   ------------------- commit 1: Agregar amigos y sortear amigo secreto -------------------
 
 
